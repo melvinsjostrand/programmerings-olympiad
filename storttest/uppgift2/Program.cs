45746810e6a274f66fec955f37321c4915d846ac
naal01002 <89377694+naal01002@users.noreply.github.com>
2021-11-09 15:09:10 +0100

    f

--- storttest/uppgift2/Program.cs
+++ storttest/uppgift2/Program.cs
@@ -17,13 +17,32 @@ class myProgram
     static void Main(string[] args)
     {
         
-        Console.WriteLine("välj ett tal mellan 1 till 5");
+        Console.WriteLine("välj hur många ord  meningen ska ha mellan 1 till 5");
         Console.ReadLine();
        Console.WriteLine("skriv något");
        string namn = Console.ReadLine();
        
-        Console.WriteLine(StringHelper.ReverseString(namn));
-        Console.WriteLine("längden på texten va " + namn.Length);
+        Console.WriteLine(StringHelper.ReverseString( namn +"  :rä gnineM"));
+       int wordCount = 0, index = 0;
+
+// skip whitespace until first word
+while (index < namn.Length && char.IsWhiteSpace(namn[index]))
+    index++;
+
+while (index < namn.Length)
+{
+    // check if current char is part of a word
+    while (index < namn.Length && !char.IsWhiteSpace(namn[index]))
+        index++;
+
+    wordCount++;
+
+    // skip whitespace until next word
+    while (index < namn.Length && char.IsWhiteSpace(namn[index]))
+        index++;
+    
+}
+Console.WriteLine(wordCount);
     }
-     }
+    }
       }
